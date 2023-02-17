bf79149ce19855080000834261b3edb0e3fafc2f
jalbu002 <110986675+jalbu002@users.noreply.github.com>
2023-02-17 09:17:08 -0500

    updated app.js

--- src/app.js
+++ src/app.js
@@ -5,10 +5,6 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-function excuseGenerator(array) {
-  return array[Math.floor(array.length * Math.random())];
-}
-
 let who = ["The dog", "My grandma", "His turtle", "My bird"];
 let action = ["ate", "peed on", "crushed", "broke"];
 let what = ["my homework", "the keys", "the car"];
@@ -20,16 +16,30 @@ let when = [
   "while I was praying"
 ];
 
+function excuseGenerator(array) {
+  return array[Math.floor(array.length * Math.random())];
+}
+
+function createString() {
+  let result =
+    excuseGenerator(who) +
+    " " +
+    excuseGenerator(action) +
+    " " +
+    excuseGenerator(what) +
+    " " +
+    excuseGenerator(when);
+  return result;
+}
+
 window.onload = function() {
   //write your code here
-  console.log(
-    excuseGenerator(who) +
-      " " +
-      excuseGenerator(action) +
-      " " +
-      excuseGenerator(what) +
-      " " +
-      excuseGenerator(when)
-  );
+  console.log();
+  console.log("Hello Rigo from the console!");
+};
+
+document.getElementById("excuse").innerHTML = createString();
+window.onload = function() {
+  //write your code here
   console.log("Hello Rigo from the console!");
 };
